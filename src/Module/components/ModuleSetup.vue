ce8bef474f8db3f9f816e73351d9304fd99d2f56
“akheel-s” <akheelmshaik@gmail.com>
2021-02-08 00:02:36 +0000

    Fixed data for setup

--- src/Module/components/ModuleSetup.vue
+++ src/Module/components/ModuleSetup.vue
@@ -14,51 +14,85 @@
         </validation-provider> -->
         <!-- BREAK -->
         <div class="module-setup__question-title">What will be their internship project?</div>
-        <v-checkbox
-          dense
-          label="Your task will be to further prototype and pilot your carry-over project and/or complete assigned tasks & projects (Recommended)"
-        ></v-checkbox>
-        <v-checkbox
-          dense
-          label="Your task will be to further prototype and pilot your carry-over project (Recommended)"
-        ></v-checkbox>
-
-        <v-checkbox
-          dense
-          label="Your task will be to further prototype, pilot and/or productize your carry-over project"
-        ></v-checkbox>
+        <validation-provider>
+          <v-checkbox
+            v-model="programDoc.data.adks[index].internshipProject1"
+            dense
+            label="Your task will be to further prototype and pilot your carry-over project and/or complete assigned tasks & projects (Recommended)"
+          ></v-checkbox>
+        </validation-provider>
+        <validation-provider>
+          <v-checkbox
+            v-model="programDoc.data.adks[index].internshipProject2"
+            dense
+            label="Your task will be to further prototype and pilot your carry-over project (Recommended)"
+          ></v-checkbox>
+        </validation-provider>
+        <validation-provider>
+          <v-checkbox
+            v-model="programDoc.data.adks[index].internshipProject3"
+            dense
+            label="Your task will be to further prototype, pilot and/or productize your carry-over project"
+          ></v-checkbox>
+        </validation-provider>
         <!-- BREAK -->
         <div class="module-setup__question-title">Is a driver's license or car required?</div>
 
-        <v-radio-group>
-          <v-radio label="No (Recommended)"></v-radio>
-          <v-radio label="Yes"></v-radio>
-        </v-radio-group>
+        <validation-provider>
+          <v-radio-group v-model="programDoc.data.adks[index].licenseRequirement">
+            <v-radio label="No (Recommended)"></v-radio>
+            <v-radio label="Yes"></v-radio>
+          </v-radio-group>
+        </validation-provider>
 
         <!-- BREAK -->
         <div class="module-setup__question-title">Who will be the employer-of-record?</div>
 
-        <v-radio-group>
+        <v-radio-group v-model="programDoc.data.adks[index].employerRecord">
           <v-radio label="My organization"></v-radio>
           <v-radio label="PilotCity (20% admin fee from total budget)"></v-radio>
         </v-radio-group>
         <!-- BREAK -->
         <div class="module-setup__question-title">What position titles can you offer?</div>
-        <v-checkbox dense label="Intern (Recommended)"></v-checkbox>
-        <v-checkbox dense label="Fellow (Recommended)"></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].intern"
+          dense
+          label="Intern (Recommended)"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].fellow"
+          dense
+          label="Fellow (Recommended)"
+        ></v-checkbox>
 
-        <v-checkbox dense label="Entrepreneur-in-Residence"></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].eir"
+          dense
+          label="Entrepreneur-in-Residence"
+        ></v-checkbox>
 
-        <v-checkbox dense label="Apprentice"></v-checkbox>
-        <v-checkbox dense label="Pre-Apprentice"></v-checkbox>
-        <v-checkbox dense label="Pre-Internship"></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].apprentice"
+          dense
+          label="Apprentice"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].preApprentice"
+          dense
+          label="Pre-Apprentice"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].preInternship"
+          dense
+          label="Pre-Internship"
+        ></v-checkbox>
 
         <!-- BREAK -->
         <div class="module-setup__question-title">
           Is there possibility of continuation or re-hire?
         </div>
 
-        <v-radio-group>
+        <v-radio-group v-model="programDoc.data.adks[index].continuation">
           <v-radio label="Yes (Recommended)"></v-radio>
           <v-radio label="No"></v-radio>
         </v-radio-group>
@@ -67,20 +101,54 @@
         <div class="module-setup__question-title">
           What compensation arrangements can you offer?
         </div>
-        <v-checkbox dense label="$250 Stipend (+20% PilotCity admin)"></v-checkbox>
-        <v-checkbox dense label="$350 Stipend (+20% PilotCity admin)"></v-checkbox>
-        <v-checkbox dense label="$500 Stipend (+20% PilotCity admin)"></v-checkbox>
-        <v-checkbox dense label="$595 Stipend (+20% PilotCity admin, Recommended)"></v-checkbox>
-        <v-checkbox dense label="W2 Employee (Minimum wage or more)"></v-checkbox>
-        <v-checkbox dense label="Unpaid ($0)"></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].compensation1"
+          dense
+          label="$250 Stipend (+20% PilotCity admin)"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].compensation2"
+          dense
+          label="$350 Stipend (+20% PilotCity admin)"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].compensation3"
+          dense
+          label="$500 Stipend (+20% PilotCity admin)"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].compensation4"
+          dense
+          label="$595 Stipend (+20% PilotCity admin, Recommended)"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].compensation5"
+          dense
+          label="W2 Employee (Minimum wage or more)"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="programDoc.data.adks[index].compensation6"
+          dense
+          label="Unpaid ($0)"
+        ></v-checkbox>
         <!-- BREAK -->
         <div class="module-setup__question-title">
           What is the minimum & maximum compensation budget?
         </div>
         <v-row class="d-flex mt-3">
-          <v-select :items="minBudget" label="Minimum" outlined></v-select>
+          <v-select
+            v-model="programDoc.data.adks[index].minimumBudget"
+            :items="minBudget"
+            label="Minimum"
+            outlined
+          ></v-select>
           <div class="ml-3 mr-3"></div>
-          <v-select :items="maxBudget" label="Maximum" outlined></v-select>
+          <v-select
+            v-model="programDoc.data.adks[index].maximumBudget"
+            :items="maxBudget"
+            label="Maximum"
+            outlined
+          ></v-select>
         </v-row>
         <!-- BREAK -->
         <div class="module-setup__question-title">
@@ -93,12 +161,11 @@
               ref="dialog"
               v-model="modalStart"
               :return-value.sync="dateStart"
-              persistent
               width="290px"
             >
               <template v-slot:activator="{ on, attrs }">
                 <v-text-field
-                  v-model="startDate"
+                  v-model="programDoc.data.adks[index].internshipStart"
                   outlined
                   label="Start Date"
                   append-icon="mdi-calendar"
@@ -107,7 +174,7 @@
                   v-on="on"
                 ></v-text-field>
               </template>
-              <v-date-picker v-model="startDate" scrollable>
+              <v-date-picker v-model="programDoc.data.adks[index].internshipStart" scrollable>
                 <v-spacer></v-spacer>
                 <v-btn text color="primary" @click="modalStart = false"> Cancel </v-btn>
                 <v-btn text color="primary" @click="$refs.dialog.save(dateStart)"> OK </v-btn>
@@ -116,16 +183,10 @@
           </v-col>
 
           <v-col class="mt-3">
-            <v-dialog
-              ref="dialog"
-              v-model="modalEnd"
-              :return-value.sync="dateEnd"
-              persistent
-              width="290px"
-            >
+            <v-dialog ref="dialog" v-model="modalEnd" :return-value.sync="dateEnd" width="290px">
               <template v-slot:activator="{ on, attrs }">
                 <v-text-field
-                  v-model="endDate"
+                  v-model="programDoc.data.adks[index].internshipEnd"
                   outlined
                   label="End Date"
                   append-icon="mdi-calendar"
@@ -134,7 +195,7 @@
                   v-on="on"
                 ></v-text-field>
               </template>
-              <v-date-picker v-model="endDate" scrollable>
+              <v-date-picker v-model="programDoc.data.adks[index].internshipEnd" scrollable>
                 <v-spacer></v-spacer>
                 <v-btn text color="primary" @click="modalEnd = false"> Cancel </v-btn>
                 <v-btn text color="primary" @click="$refs.dialog.save(dateEnd)"> OK </v-btn>
@@ -161,9 +222,19 @@
 
         <div class="module-setup__question-title">How many days & hours per week?</div>
         <v-row class="d-flex mt-3">
-          <v-select :items="daysPerWeek" label="Days per week?" outlined></v-select>
+          <v-select
+            v-model="programDoc.data.adks[index].daysPerWeek"
+            :items="daysPerWeek"
+            label="Days per week?"
+            outlined
+          ></v-select>
           <div class="ml-3 mr-3"></div>
-          <v-select :items="hoursPerDay" label="Hours per day?" outlined></v-select>
+          <v-select
+            v-model="programDoc.data.adks[index].hoursPerDay"
+            :items="hoursPerDay"
+            label="Hours per day?"
+            outlined
+          ></v-select>
         </v-row>
         <!-- BREAK -->
         <div class="module-setup__question-title">
@@ -174,7 +245,7 @@
           <v-dialog ref="dialog" v-model="modal" :return-value.sync="date" persistent width="290px">
             <template v-slot:activator="{ on, attrs }">
               <v-text-field
-                v-model="acceptanceDeadline"
+                v-model="programDoc.data.adks[index].acceptanceDeadline"
                 outlined
                 label="Acceptance deadline"
                 append-icon="mdi-calendar"
@@ -183,7 +254,7 @@
                 v-on="on"
               ></v-text-field>
             </template>
-            <v-date-picker v-model="acceptanceDeadline" scrollable>
+            <v-date-picker v-model="programDoc.data.adks[index].acceptanceDeadline" scrollable>
               <v-spacer></v-spacer>
               <v-btn text color="primary" @click="modal = false"> Cancel </v-btn>
               <v-btn text color="primary" @click="$refs.dialog.save(date)"> OK </v-btn>
@@ -235,18 +306,31 @@ export default defineComponent({
     const initResearchSetup = {
       research: [
         {
-          internshipProject: '',
+          internshipProject1: '',
+          internshipProject2: '',
+          internshipProject3: '',
           licenseRequirement: '',
           employerRecord: '',
-          positionTitle: '',
+          intern: '',
+          fellow: '',
+          eir: '',
+          apprentice: '',
+          preApprentice: '',
+          preInternship: '',
           continuation: '',
-          compensation: '',
+          compensation1: '',
+          compensation2: '',
+          compensation3: '',
+          compensation4: '',
+          compensation5: '',
+          compensation6: '',
           minimumBudget: '',
           maximumBudget: '',
           internshipStart: '',
           internshipEnd: '',
           daysPerWeek: '',
           hoursPerDay: '',
+          acceptanceDeadline: '',
           required: false
         }
       ]
