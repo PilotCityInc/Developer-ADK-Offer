a44708e29db57dab4902dc40609e599737fe6d66
juxsta <ericjrreyes2@gmail.com>
2021-02-19 10:08:01 -0800

    removed keep alive

--- src/Module/Module.vue
+++ src/Module/Module.vue
@@ -102,7 +102,7 @@
           </div>
         </div>
         <div class="module__page">
-          <keep-alive> <component :is="getComponent" v-model="programDoc" /> </keep-alive>
+          <component :is="getComponent" v-model="programDoc" />
         </div>
       </div>
     </div>
